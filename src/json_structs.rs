e6e27ae75c4526fba0f33880bd8f9b0b05fad46d
Miles Granger <miles59923@gmail.com>
2018-03-16 08:46:04 +0100

    initial swap to enum return

--- src/json_structs.rs
+++ src/json_structs.rs
@@ -2,6 +2,7 @@ use std::str::FromStr;
 use std::str::Utf8Error;
 use rocket::request;
 use rocket::http;
+use elevation;
 
 /// Struct to represent a JSON query parameter for a given location
 #[derive(FromForm, Serialize, Deserialize)]
@@ -9,13 +10,19 @@ pub struct Points {
     pub points: CoordinateList
 }
 
+
+/// Struct to represent the core value of the Points struct
+/// list of tuples representing (lat, lon) values
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CoordinateList(pub Vec<(f64, f64)>);
 
-impl FromStr for CoordinateList {
-    type Err = Utf8Error;
 
-    // "?points=(45.9,34.8)(34.3,28.9)".split('(') -> ["45.9,34.8)", "34.3,28.9)"].map(
+/// Implement FromStr for CoordinateList to parse the coordinate list from the request query
+impl FromStr for CoordinateList {
+
+    type Err = Utf8Error;  // TODO: This probably isn't the correct error here...
+
+    // Take a list of tuples ie. (12.23,45.45),(23.3,34.5) and form into Vec<f64, f64)> data type
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let points: Vec<&str> = s.split("(").filter(|v| v != &"").collect::<Vec<&str>>();
         let points: Vec<&str> = points.iter().map(|v| v.trim_matches(|v|v == ')' || v == ',')).collect();
@@ -30,6 +37,8 @@ impl FromStr for CoordinateList {
 }
 
 
+/// Implement for Rocket to parse the value from the request, which will implicitly invoke the
+/// FromStr impl above.
 impl<'v> request::FromFormValue<'v> for CoordinateList {
     type Error = &'v http::RawStr;
 
@@ -39,4 +48,12 @@ impl<'v> request::FromFormValue<'v> for CoordinateList {
             _ => Err(form_value)
         }
     }
+}
+
+
+/// Enum to return
+#[derive(Serialize)]
+pub enum Message {
+    Success {message: Vec<elevation::Elevation>},
+    Error {message: String}
 }
--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,45 @@ pub extern crate netcdf;
 pub extern crate ndarray;
 pub extern crate glob;
 pub extern crate serde;
-pub extern crate serde_json;
+#[macro_use] pub extern crate serde_json;
 #[macro_use] pub extern crate serde_derive;
 
-use std::f64;
-use std::str::FromStr;
 use glob::glob;
 use rocket_contrib::Json;
 
+// Local mods
 #[cfg(test)]
 mod tests;
 mod elevation;
 mod json_structs;
-use json_structs::{Points};
+use json_structs::{Points, Message};
 
+
+// Sanity check
 #[get("/")]
 fn index() -> &'static str {
     "Up and running!"
 }
 
 
+
+// Main API for 90m resolution
 #[get("/api/elevations/90m?<points>")]
-fn get_elevations(points: Points) -> Json<Vec<elevation::Elevation>> {
-    let metas = elevation::load_summary_file();
-    let elevations = elevation::get_elevations(points.points.0, &metas);
-    Json(elevations)
+fn get_elevations(points: Option<Points>) -> Json<Message> {
+
+    match points {
+        Some(points) => {
+            let metas = elevation::load_summary_file();
+            let elevations = elevation::get_elevations(points.points.0, &metas);
+            Json(Message::Success{message: elevations})
+        },
+        None => {
+            Json(Message::Error{message: "Failed to parse request".to_string()})
+        }
+    }
+
 }
+
 
 fn main() {
     rocket::ignite()
